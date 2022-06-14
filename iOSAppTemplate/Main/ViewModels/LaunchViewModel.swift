7acf4c3fa96394edb659eb72fe7a028601233498
Jhonatan1505 <josue-el-rey14@hotmail.com>
2022-06-13 23:20:19 -0500

    Laboratorio 13

---
+++ iOSAppTemplate/Main/ViewModels/LaunchViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchViewModel.swift
+//  iOSAppTemplate
+//
+//  Created by MAC36 on 13/06/22.
+//
+
+import Foundation
+
+class LaunchViewModel : ObservableObject {
+    
+    let appState = AppState.shared
+    
+    init() {
+
+        appState.currentScreen = .home
+    }
+}
